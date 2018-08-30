9d4089f8206dd9851eb220f4c0c10077ccb502de
Abhishek Aryan <abhishek.rbg@gmail.com>
2018-08-30 19:58:45 +0530

    do some shit

--- app.js
+++ app.js
@@ -1,31 +1,40 @@
-$("#some").html("It's\nFucking\n<span id=with-anim>BBQ </span>\ntime");
 (function () {
   //Weather Function
   function weather() {
     var location = document.getElementById("location");
     var apiKey = "99205d9d8c768513f61624c90c83f0e0";
     var url = "https://api.forecast.io/forecast/";
+    var proxy = "https://cors-anywhere.herokuapp.com/"
 
     navigator.geolocation.getCurrentPosition(success, error);
 
     function success(position) {
     latitude = position.coords.latitude;
     longitude = position.coords.longitude;
-
-    location.innerHTML =
-      "Latitude is " + latitude + "° Longitude is " + longitude + "°";
-      $.getJSON(url + apiKey + '/' + latitude + ',' + longitude, function(forecast) {
-        console.log(forecast);
-    });
+    currenttemp="";
+    currenticon="";
+    var cdt;
+    $.getJSON(proxy + url + apiKey + '/' + latitude + ',' + longitude + '?' + "units=si", function(forecast) {
+      currenticon = forecast.currently.icon;
+      currenttemp = Math.round(forecast.currently.temperature);
+      cdt = function (currenticon){
+        if(forecast.currently.icon === "partly-cloudy-day" || "partly-cloudy-night" )
+          return "cloudy";
+        else
+          return currenticon;
+      }();
+      $("#some").html("It's\nFucking "+currenttemp+"\n<span id=with-anim>"+cdt+"</span>\ntime");
+  });
   }
-
   function error() {
-    location.innerHTML = "Unable to retrieve your location";
+    some.innerHTML = "Unable to retrieve your location";
   }
-
-  location.innerHTML = "Locating...";
 }
-
+  function parser(fcast){
+    $("#some").html("It's\nFucking\n<span id=with-anim>hi</span>\n");
+    //use replace() subsitute for display
+  }
+  
   weather();
 
 })();
---
+++ portedtojs.js
@@ -0,0 +1,584 @@
+allphrase =[
+    {
+        highlight:"love is",
+        title:"Fucking#CRLF#love is#CRLF#in the air.",
+        subline:"Take off your shirt and get wet.",
+        min:25,
+        condition:"clear",
+        color:"FF0060"
+    },
+    {
+        highlight:"fucking",
+        title:"It's#CRLF#fucking#CRLF#raining.",
+        subline:"You can look outside to get more information.",
+        condition:"rain",
+        color:"007EFF"
+    },
+    {
+        highlight:"fucking",
+        title:"It's#CRLF#fucking#CRLF#cloudy.",
+        subline:"You can look outside to get more information.",
+        condition:"cloudy",
+        color:"007EFF"
+    },
+    {
+        highlight:"fridge.",
+        title:"Freezing#CRLF#cold like#CRLF#a fucking#CRLF#fridge.",
+        subline:"You can look outside to get more information.",
+        max:4,
+        color:"004A96"
+    },
+    {
+        highlight:"fucking",
+        title:"It's#CRLF#fucking#CRLF#nice.",
+        subline:"Take off your shirt but keep on your panties.",
+        min:20,
+        max:30,
+        color:"FF0060"
+    },
+    {
+        highlight:"fucking",
+        title:"Holy#CRLF#fucking#CRLF#snow.",
+        subline:"Still not ready for building that fucking snow castle.",
+        condition:"snow",
+        color:"007EFF"
+    },
+    {
+        color:"007EFF",
+        condition:"clear",
+        min:5,
+        title:"It's#CRLF#fucking#CRLF#alright#CRLF#today.",
+        subline:"Not amazeballs but also not fucking shitty.",
+        highlight:"fucking",
+        max:15
+    },
+    {
+        highlight:"freezing",
+        title:"Are you#CRLF#freezing#CRLF#fucking#CRLF#serious?",
+        subline:"You can't look outside because of fucking snow.",
+        max:3,
+        condition:"snow",
+        color:"007EFF"
+    },
+    {
+        highlight:"oven.",
+        title:"It's like#CRLF#a fucking#CRLF#oven.",
+        subline:"Just get naked, now - And get some ice cream.",
+        min:30,
+        color:"FFC600"
+    },
+    {
+        color:"9F9F9F",
+        condition:"cloudy",
+        min:5,
+        title:"It's just#CRLF#fucking#CRLF#grey.",
+        subline:"Just clouds & no love, but computer games.",
+        highlight:"grey.",
+        max:16
+    },
+    {
+        highlight:"fucking",
+        title:"Can't see#CRLF#because#CRLF#fucking#CRLF#snow.",
+        subline:"Try looking outside for more information, try it...",
+        condition:"snow",
+        color:"FFFFFF"
+    },
+    {
+        highlight:"fucking",
+        title:"Can't see#CRLF#because#CRLF#fucking#CRLF#snow.",
+        subline:"Try looking outside for more information, try it...",
+        condition:"sleet",
+        color:"FFFFFF"
+    },
+    {
+        color:"FF0060",
+        condition:"clear",
+        min:20,
+        title:"Fucking#CRLF#Amaze#CRLF#Balls.",
+        subline:"So fucking nice outside, holy schmoly.",
+        highlight:"Amaze",
+        max:35
+    },
+    {
+        highlight:"fucking",
+        title:"I'm#CRLF#fucking#CRLF#melting.",
+        subline:"Take off your shirt and get wet.",
+        min:23,
+        color:"FF0000"
+    },
+    {
+        highlight:"fucking",
+        title:"It's so#CRLF#fucking#CRLF#hot.",
+        subline:"I wouldn't go outside for more information.",
+        min:25,
+        color:"FF0000"
+    },
+    {
+        highlight:"fucking",
+        title:"It's#CRLF#fucking#CRLF#car wash#CRLF#time.",
+        subline:"Exactly! It's going to be hot as shit.",
+        min:24,
+        color:"FF0F66"
+    },
+    {
+        highlight:"fucking",
+        title:"Let's get#CRLF#fucking#CRLF#naked.",
+        subline:"You heard it, it's going to be hot!",
+        min:24,
+        color:"FF0F66"
+    },
+    {
+        highlight:"fucking",
+        title:"Global#CRLF#fucking#CRLF#warming.",
+        subline:"Yeah, let's blame Global Warming.",
+        min:30,
+        color:"FF0000"
+    },
+    {
+        highlight:"fucking",
+        title:"It's#CRLF#fucking#CRLF#tropical.",
+        subline:"Ice Cream is my final answer.",
+        min:35,
+        color:"FF0000"
+    },
+    {
+        highlight:"damn",
+        title:"It's too#CRLF#damn#CRLF#hot.",
+        subline:"Ice Cream is my final answer.",
+        min:33,
+        color:"FF0000"
+    },
+    {
+        highlight:"you are",
+        title:"I think#CRLF#you are#CRLF#fucking#CRLF#hot.",
+        subline:"Just kidding, but the fucking weather is.",
+        min:25,
+        color:"FF0000"
+    },
+    {
+        highlight:"hot.",
+        title:"Drop it#CRLF#like it's#CRLF#hot.",
+        subline:"*Don't drop your fucking iPhone though, seriously.",
+        min:35,
+        color:"FF0000"
+    },
+    {
+        highlight:"BBQ",
+        title:"It's#CRLF#fucking#CRLF#BBQ#CRLF#time.",
+        subline:"Vegetarians and Meat fans unite! BBQ is for all of us.",
+        min:24,
+        color:"FF0000"
+    },
+    {
+        highlight:"umbrella.",
+        title:"Get your#CRLF#fucking#CRLF#umbrella.",
+        subline:"Shitloads of rain is awaiting you.",
+        condition:"rain",
+        color:"007EFF"
+    },
+    {
+        highlight:"shades",
+        title:"Fucking#CRLF#fifty#CRLF#shades#CRLF#of grey.",
+        subline:"Fucking grey clouds everywhere.",
+        condition:"cloudy",
+        color:"9F9F9F"
+    },
+    {
+        highlight:"thunder",
+        title:"Fucking#CRLF#thunder#CRLF#storm.",
+        subline:"It's time to sing the Fuck you Thunder song.",
+        condition:"wind",
+        color:"9F9F9F"
+    },
+    {
+        highlight:"Cloudy",
+        title:"Cloudy#CRLF#with a#CRLF#chance#CRLF#of....",
+        subline:"You thought I would say meatballs right? Stupid.",
+        condition:"cloudy",
+        color:"007EFF"
+    },
+    {
+        highlight:"fucking",
+        title:"It's#CRLF#getting#CRLF#fucking#CRLF#dark.",
+        subline:"The storm is coming - May the Force be with you.",
+        condition:"wind",
+        color:"9F9F9F"
+    },
+    {
+        highlight:"stoopid.",
+        title:"So hot#CRLF#makin'#CRLF#me#CRLF#stoopid.",
+        subline:"I heard that Ryan Gosling said this.",
+        min:34,
+        color:"FF0000"
+    },
+    {
+        highlight:"stupid",
+        title:"It's#CRLF#stupid#CRLF#hot.",
+        subline:"Even fucking Siri has no answer for you today.",
+        min:25,
+        color:"FF0000"
+    },
+    {
+        highlight:"whiskey",
+        title:"It's#CRLF#fucking#CRLF#whiskey#CRLF#time.",
+        subline:"This will warm you up. It's freezing!",
+        max:5,
+        color:"004A96"
+    },
+    {
+        highlight:"skiing",
+        title:"It's#CRLF#fucking#CRLF#skiing#CRLF#time.",
+        subline:"Snow every where, yay!",
+        condition:"snow",
+        color:"004A96"
+    },
+    {
+        highlight:"monkey",
+        title:"Cold ass#CRLF#monkey#CRLF#pants.",
+        subline:"Yeah, exactly.",
+        max:5,
+        color:"004A96"
+    },
+    {
+        highlight:"cold",
+        title:"Hey girl#CRLF#are you#CRLF#cold?",
+        subline:"That's because it's freezing outside. Go & look.",
+        max:5,
+        color:"004A96"
+    },
+    {
+        highlight:"Hello?",
+        title:"Hello?#CRLF#yes, this#CRLF#is snow-#CRLF#man.",
+        subline:"It's time to build me. Look outside fancy pants.",
+        condition:"snow",
+        color:"004A96"
+    },
+    {
+        highlight:"fucking",
+        title:"Frozen#CRLF#fucking#CRLF#weather.",
+        subline:"It's fucking freezing right now, look outside.",
+        max:5,
+        color:"004A96"
+    },
+    {
+        highlight:"fucking",
+        title:"Negative#CRLF#fucking#CRLF#zero.",
+        subline:"It's fucking freezing right now, look outside!",
+        max:0,
+        color:"004A96"
+    },
+    {
+        highlight:"fucking",
+        title:"Below#CRLF#fucking#CRLF#zero.",
+        subline:"It's fucking freezing right now, look outside!",
+        max:-1,
+        color:"004A96"
+    },
+    {
+        highlight:"fucking",
+        title:"Cold as#CRLF#fucking#CRLF#shit.",
+        subline:"You heard it, unpack your space heater!",
+        max:4,
+        color:"004A96"
+    },
+    {
+        highlight:"fucking",
+        title:"It's#CRLF#getting#CRLF#fucking#CRLF#chilly.",
+        subline:"You heard it, unpack your space heater!",
+        min:5,
+        max:10,
+        color:"004A96"
+    },
+    {
+        highlight:"fucking",
+        title:"Frozen#CRLF#fucking#CRLF#fingers.",
+        subline:"Can't feel my fingers any more.",
+        max:0,
+        color:"004A96"
+    },
+    {
+        highlight:"fucking",
+        title:"Frost#CRLF#fucking#CRLF#bite.",
+        subline:"Can't feel my fingers any more.",
+        max:0,
+        color:"004A96"
+    },
+    {
+        highlight:"fucking",
+        title:"As cold#CRLF#as a#CRLF#fucking#CRLF#penguin.",
+        subline:"I have to admit, that sounds kinda cute.",
+        max:0,
+        color:"004A96"
+    },
+    {
+        highlight:"fucking",
+        title:"North#CRLF#fucking#CRLF#pole.",
+        subline:"Nothing against the north pole, but seriously!",
+        max:0,
+        color:"004A96"
+    },
+    {
+        highlight:"your shit.",
+        title:"Icebergs#CRLF#all over#CRLF#your shit.",
+        subline:"Time to get cozy and watch Titanic again!",
+        max:5,
+        color:"004A96"
+    },
+    {
+        highlight:"ice age.",
+        title:"Fucking#CRLF#ice age.",
+        subline:"The movie was great, the weather is not.",
+        max:0,
+        color:"004A96"
+    },
+    {
+        highlight:"fucking",
+        title:"It's#CRLF#fucking#CRLF#arctic.",
+        subline:"Can't feel my fucking fingers any more.",
+        max:0,
+        color:"004A96"
+    },
+    {
+        highlight:"fucking",
+        title:"Glacial#CRLF#fucking#CRLF#ice.",
+        subline:"Can't feel my fucking fingers any more.",
+        max:0,
+        color:"004A96"
+    },
+    {
+        highlight:"fucking",
+        title:"Numb#CRLF#fucking#CRLF#fingers.",
+        subline:"Can't feel my fucking fingers any more.",
+        max:3,
+        color:"004A96"
+    },
+    {
+        highlight:"fucking",
+        title:"Hypo#CRLF#fucking#CRLF#thermic.",
+        subline:"A friend of mine called Tim said that. Smartypants.",
+        max:-3,
+        color:"004A96"
+    },
+    {
+        highlight:"nipples",
+        title:"Am I#CRLF#allowed#CRLF#to say#CRLF#nipples?",
+        subline:"Because it's freezing! You get the point.",
+        max:5,
+        color:"004A96"
+    },
+    {
+        highlight:"fucking",
+        title:"Brezzy#CRLF#fucking#CRLF#breezy.",
+        subline:"It's getting cold outside!",
+        max:6,
+        color:"004A96"
+    },
+    {
+        highlight:"fucking",
+        title:"It's#CRLF#fucking#CRLF#foggy.",
+        subline:"It's getting cold outside!",
+        condition:"fog",
+        color:"004A96"
+    },
+    {
+        highlight:"fucking",
+        title:"It's#CRLF#fucking#CRLF#windy.",
+        subline:"It's getting cold outside!",
+        condition:"wind",
+        color:"004A96"
+    },
+    {
+        highlight:"shitty",
+        title:"Totally#CRLF#not#CRLF#shitty.",
+        subline:"Look outside for more information.",
+        min:10,
+        condition:"cloudy",
+        color:"004A96"
+    },
+    {
+        highlight:"fucking",
+        title:"It's#CRLF#fucking#CRLF#okay.",
+        subline:"Look outside for more information.",
+        min:9,
+        max:20,
+        color:"004A96"
+    },
+    {
+        highlight:"ducking",
+        title:"It's#CRLF#ducking#CRLF#freezing.",
+        subline:"Oh damnit you fucking autocorrect.",
+        max:3,
+        color:"004A96"
+    },
+    {
+        highlight:"nippy!",
+        title:"It's a#CRLF#wee bit #CRLF#nippy!",
+        subline:"This is for you my Scottish friends.",
+        min:-3,
+        max:6,
+        color:"004A96"
+    },
+    {
+        highlight:"frosty",
+        title:"It's#CRLF#frosty#CRLF#as fuck.",
+        subline:"This is for you my Scottish friends.",
+        max:0,
+        color:"004A96"
+    },
+    {
+        highlight:"It rains",
+        title:"It rains#CRLF#cats and#CRLF#dogs.",
+        subline:"You better get an umbrella my friend.",
+        condition:"rain",
+        color:"004A96"
+    },
+    {
+        highlight:"for chest",
+        title:"Prepare#CRLF#for chest#CRLF#hair.",
+        subline:"It's fucking freezing, boys & girls!",
+        max:0,
+        color:"004A96"
+    },
+    {
+        highlight:"freeze.",
+        title:"Fucking#CRLF#brain#CRLF#freeze.",
+        subline:"Fuck Yeah Ice Cream!",
+        min:20,
+        color:"FF0000"
+    },
+    {
+        highlight:"glasses.",
+        title:"Sun#CRLF#fucking#CRLF#glasses.",
+        subline:"Time to put on my coolest shades.",
+        min:15,
+        condition:"clear",
+        color:"FF0000"
+    },
+    {
+        highlight:"fried.",
+        title:"Let's get#CRLF#fucking#CRLF#fried.",
+        subline:"Take off your shirt but keep on the panties.",
+        min:20,
+        color:"FF0000"
+    },
+    {
+        highlight:"fucking",
+        title:"I'm on a#CRLF#fucking#CRLF#boat.",
+        subline:"It's hot! Take off your shirt, keep on your pants.",
+        min:20,
+        color:"FF0000"
+    },
+    {
+        highlight:"in bed.",
+        title:"Meh...#CRLF#Just stay#CRLF#in bed.",
+        subline:"You can browse Tumblr & play Angrybirds.",
+        condition:"rain",
+        color:"004A96"
+    },
+    {
+        highlight:"in bed.",
+        title:"Meh...#CRLF#Just stay#CRLF#in bed.",
+        subline:"You can browse Tumblr & play Angrybirds.",
+        condition:"sleet",
+        color:"004A96"
+    },
+    {
+        highlight:"in bed.",
+        title:"Meh...#CRLF#Just stay#CRLF#in bed.",
+        subline:"You can browse Tumblr & play Angrybirds.",
+        min:10,
+        condition:"cloudy",
+        color:"004A96"
+    },
+    {
+        highlight:"in bed.",
+        title:"Meh...#CRLF#Just stay#CRLF#in bed.",
+        subline:"You can browse Tumblr & play Angrybirds.",
+        min:15,
+        condition:"fog",
+        color:"004A96"
+    },
+    {
+        highlight:"beard.",
+        title:"Grow a#CRLF#fucking#CRLF#beard.",
+        subline:"It's freezing outside, you will need it.",
+        max:4,
+        color:"004A96"
+    },
+    {
+        highlight:"amazing",
+        title:"You look#CRLF#amazing#CRLF#today.",
+        subline:"And so is the weather. You just need to believe it.",
+        min:15,
+        max:20,
+        color:"FF0F66"
+    },
+    {
+        highlight:"amazing",
+        title:"You look#CRLF#amazing#CRLF#today.",
+        subline:"And so is the weather.",
+        min:15,
+        condition:"clear",
+        color:"FF0F66"
+    },
+    {
+        color:"FE730F",
+        condition:"cloudy",
+        min:3,
+        title:"It’s like#CRLF#a meh…#CRLF#kinda#CRLF#day.",
+        subline:"But you can change it with a smile :) Or drugs.",
+        highlight:"kinda",
+        max:9
+    }
+];
+
+function getphrase()
+    condition = SKIN:GetMeasure('MeasureIcon'):GetStringValue()
+    temp = SKIN:GetMeasure('MeasureTempC'):GetValue()
+    possiblePhrases = {}
+    for k,v in ipairs(phrase) do
+        if v['condition'] then
+            if string.match(v['condition'],condition) then
+                if v['min'] then
+                    if temp >= v['min'] then
+                        if v['max'] then
+                            if temp <= v['max'] then
+                                table.insert(possiblePhrases,k)
+                            end
+                        else
+                            table.insert(possiblePhrases,k)
+                        end
+                    end
+                elseif v['max'] then
+                    if temp <= v['max'] then
+                        table.insert(possiblePhrases,k)
+                    end
+                else
+                    table.insert(possiblePhrases,k)
+                end
+            end
+        else
+            if v['min'] then
+                if temp >= v['min'] then
+                    if v['max'] then
+                        if temp <= v['max'] then
+                            table.insert(possiblePhrases,k)
+                        end
+                    else
+                        table.insert(possiblePhrases,k)
+                    end
+                end
+            elseif v['max'] then
+                if temp <= v['max'] then
+                    table.insert(possiblePhrases,k)
+                end
+            end
+        end
+    end
+    sorandom = possiblePhrases[math.random(1,#possiblePhrases)]
+    SKIN:Bang('!SetOption MeterString Text "'..phrase[sorandom]['title'])
+    SKIN:Bang('!SetOption MeterString InlinePattern "'..phrase[sorandom]['highlight'])
+    SKIN:Bang('!SetOption MeterString InlineSetting "Color|'..phrase[sorandom]['color'])
+    SKIN:Bang('!SetOption Subline Text "'..phrase[sorandom]['subline'])
+    SKIN:Bang('[!SetOption Icon ImageName "#@#Icon\\#*IconColor*#\\[*MeasureIcon*][!ShowMeter TempText][!UpdateMeterGroup Text][!UpdateMeterGroup Text]')
+end
