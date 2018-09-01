5234f6c802fc50f7d8631f38205dbed777f5bbdc
Abhishek Aryan <abhishek.rbg@gmail.com>
2018-09-01 19:14:02 +0530

    ADD pinch code
    pretty seghattii

--- app.js
+++ app.js
@@ -10,7 +10,7 @@
             "Sacrificing a goat..."
         ];
         //$("#some").css(propertyName, value);
-        $("#loading").text(loadtext[Math.floor(Math.random()*loadtext.length)]);
+        $("#loading").text(loadtext[Math.floor(Math.random() * loadtext.length)]);
         $("#some").hide(0);
     })();
     //Global Variables
@@ -23,7 +23,7 @@
         var location = document.getElementById("location");
         var apiKey = "99205d9d8c768513f61624c90c83f0e0";
         var url = "https://api.forecast.io/forecast/";
-        var proxy = "https://cors-anywhere.herokuapp.com/"
+        var proxy = "https://cors-anywhere.herokuapp.com/";
         navigator.geolocation.getCurrentPosition(success, error);
         //for geolocation succ
         function success(position) {
@@ -31,26 +31,33 @@
             longitude = position.coords.longitude;
             currenticon = "";
             //request for the json for weather
-            $.getJSON(proxy + url + apiKey + '/' + latitude + ',' + longitude + '?' + "units=si", function (forecast) {
-                currenticon = forecast.currently.icon;
-                ctemp = Math.round(forecast.currently.temperature);
-                cdt = function (currenticon) {
-                    if (forecast.currently.icon === "partly-cloudy-day" || "partly-cloudy-night")
-                        return "cloudy";
-                    else
-                        return currenticon;
-                }();
-                //hide loading
-                $("#loading").fadeOut(200);
-                //Construsct the phrase via the parser
-                cphrase = phrasemaker((parser(cdt, ctemp)));
-                $("#some").html(cphrase.title);
-                //set the color for the highlight
-                $("#with-anim").css({ "background-image": cphrase.effcolor});
-                setTimeout(function(){
-                    $("#some").fadeIn(600);
-                }, 300);
-            });
+            $.getJSON(
+                proxy + url + apiKey + "/" + latitude + "," + longitude + "?units=si",
+                function (forecast) {
+                    currenticon = forecast.currently.icon;
+                    ctemp = Math.round(forecast.currently.temperature);
+                    cdt = (function (currenticon) {
+                        if (
+                            forecast.currently.icon === "partly-cloudy-day" ||
+                            "partly-cloudy-night"
+                        )
+                            return "cloudy";
+                        else return currenticon;
+                    })();
+                    init();
+                    //hide loading
+                    $("#loading").fadeOut(200);
+                    //Construsct the phrase via the parser
+                    cphrase = phrasemaker(parser(cdt, ctemp));
+                    $("#some").html(cphrase.title);
+                    //set the color for the highlight
+                    $("#with-anim").css({ "background-image": cphrase.effcolor });
+                    setTimeout(function () {
+                        $("#some").fadeIn(600);
+                    }, 300);
+                    beingReplaced= $("#some").html();
+                }
+            );
         }
         function error() {
             some.innerHTML = "Unable to retrieve your location";
@@ -601,18 +608,18 @@
                             if (temp >= v.min) {
                                 if (v.max) {
                                     if (temp <= v.max) {
-                                        possiblePhrase.push(k)
+                                        possiblePhrase.push(k);
                                     }
                                 } else {
-                                    possiblePhrase.push(k)
+                                    possiblePhrase.push(k);
                                 }
                             }
                         } else if (v.max) {
                             if (temp <= v.max) {
-                                possiblePhrase.push(k)
+                                possiblePhrase.push(k);
                             }
                         } else {
-                            possiblePhrase.push(k)
+                            possiblePhrase.push(k);
                         }
                     }
                 } else {
@@ -620,37 +627,125 @@
                         if (temp >= v.min) {
                             if (v.max) {
                                 if (temp <= v.max) {
-                                    possiblePhrase.push(k)
+                                    possiblePhrase.push(k);
                                 }
                             } else {
-                                possiblePhrase.push(k)
+                                possiblePhrase.push(k);
                             }
                         }
                     } else if (v.max) {
                         if (temp <= v.max) {
-                            possiblePhrase.push(k)
+                            possiblePhrase.push(k);
                         }
                     }
                 }
             });
             const random = Math.round(Math.random() * (possiblePhrase.length - 1));
             const yourPhrase = phrases[possiblePhrase[random]];
-            return (yourPhrase);
+            return yourPhrase;
         }
         return getPhrase(con, temp);
     }
-
-    weather();
     //Phrasemaker 2000-inator
     function phrasemaker(ph) {
         var processed = {
-            title:"",
+            title: "",
             effcolor: ""
         };
         var highlighted = "<span id=with-anim>" + ph.highlight + "</span>";
         processed.title = ph.title.replace(ph.highlight, highlighted);
         processed.title += "\n<span id=subtext>\n" + ph.subline + "</span>";
-        processed.effcolor = 'linear-gradient(45deg, #dadadada 20%, #' + ph.color + ' 40%, #' + ph.color + ' 60%, #dadadada 80%)';
+        processed.effcolor = "linear-gradient(45deg, #dadadada 20%, #" + ph.color + " 40%, #" + ph.color + " 60%, #dadadada 80%)";
         return processed;
+    }
+
+    weather();
+
+    {
+        // Global vars to cache event state
+        var evCache = new Array();
+        var prevDiff = -1;
+
+        function pointerdown_handler(ev) {
+            // The pointerdown event signals the start of a touch interaction.
+            // This event is cached to support 2-finger gestures
+            evCache.push(ev);
+        }
+        function pointermove_handler(ev) {
+            // This function implements a 2-pointer horizontal pinch/zoom gesture.
+            //
+            // If the distance between the two pointers has increased (zoom in),
+            // the taget element's background is changed to "pink" and if the
+            // distance is decreasing (zoom out), the color is changed to "lightblue".
+            //
+            // This function sets the target element's border to "dashed" to visually
+            // indicate the pointer's target received a move event.
+            // Find this event in the cache and update its record with this event
+            for (var i = 0; i < evCache.length; i++) {
+                if (ev.pointerId == evCache[i].pointerId) {
+                    evCache[i] = ev;
+                    break;
+                }
+            }
+            // If two pointers are down, check for pinch gestures
+            if (evCache.length == 2) {
+                // Calculate the distance between the two pointers
+                var curDiff = Math.abs(evCache[0].clientX - evCache[1].clientX);
+                if (prevDiff > 0) {
+                    if (curDiff > prevDiff) {
+                        // The distance between the two pointers has increased
+                        showTemp();
+                    }
+                    if (curDiff < prevDiff) {
+                        // The distance between the two pointers has decreased
+                        hideTemp();;
+                    }
+                }
+                // Cache the distance for the next move event
+                prevDiff = curDiff;
+            }
+        }
+        function pointerup_handler(ev) {
+            // Remove this pointer from the cache and reset the target's
+            // background and border
+            remove_event(ev);
+            // If the number of pointers down is less than two then reset diff tracker
+            if (evCache.length < 2) prevDiff = -1;
+        }
+        function remove_event(ev) {
+            // Remove this event from the target's cache
+            for (var i = 0; i < evCache.length; i++) {
+                if (evCache[i].pointerId == ev.pointerId) {
+                    evCache.splice(i, 1);
+                    break;
+                }
+            }
+        }
+        function init() {
+            // Install event handlers for the pointer target
+            var el = document.querySelector(".container");
+            el.onpointerdown = pointerdown_handler;
+            el.onpointermove = pointermove_handler;
+            // Use same handler for pointer{up,cancel,out,leave} events since
+            // the semantics for these events - in this app - are the same.
+            el.onpointerup = pointerup_handler;
+            el.onpointercancel = pointerup_handler;
+            el.onpointerout = pointerup_handler;
+            el.onpointerleave = pointerup_handler;
+        }
+    }
+    var beingReplaced;
+    function showTemp(){
+        $("#some").fadeOut();
+        $("#some").html("It's\nFucking\n<span id=with-anim>" + ctemp + "º!</span>");
+        $("#with-anim").css({ "background-image": cphrase.effcolor });
+        $("#some").fadeIn();
+
+    }
+    function hideTemp(){
+        $("#some").fadeOut();
+        $("#some").html(beingReplaced);
+        //$("#with-anim").css({ "background-image": cphrase.effcolor });
+        $("#some").fadeIn();
     }
 })();
