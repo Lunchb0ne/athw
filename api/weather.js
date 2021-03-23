3f8a77f1516b81e9b32ea8bd78c4a42f25cfe4c7
Abhishek Aryan <abhishek.rbg@gmail.com>
2021-03-23 23:10:14 +0300

    Add api

--- api/weather.js
+++ api/weather.js
@@ -1,4 +1,36 @@
-module.exports = (req, res) => {
-	const { name = 'World' } = req.query;
-	res.status(200).send(`Hello ${name}!`);
+const https = require('https');
+const initial = 'https://api.forecast.io/forecast/' + process.env.DARKSKY + '/';
+
+module.exports = async (req, result) => {
+	result.setHeader('Access-Control-Allow-Credentials', true);
+	result.setHeader('Access-Control-Allow-Origin', '*');
+	// another common pattern
+	// res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+	result.setHeader(
+		'Access-Control-Allow-Methods',
+		'GET,OPTIONS,PATCH,DELETE,POST,PUT'
+	);
+	result.setHeader(
+		'Access-Control-Allow-Headers',
+		'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+	);
+	const { latitude = 1, longitude = 1 } = req.query;
+	// console.log(req.query);
+	const assembler = initial + latitude + ',' + longitude + '?' + 'units=si';
+	// console.log(assembler);
+	https
+		.get(assembler, function (res) {
+			var body = '';
+
+			res.on('data', function (chunk) {
+				body += chunk;
+			});
+
+			res.on('end', function () {
+				result.status(200).send(body);
+			});
+		})
+		.on('error', function (e) {
+			result.status(403).send('Got an error: ', e);
+		});
 };
